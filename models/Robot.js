cbe6be34957102114e31b400a684f2afa546f2b6
alexiscg <alexis.corbacho@hotmail.com>
2020-09-19 23:31:22 +0200

    Robot model changed to prototypes when needed

--- models/Robot.js
+++ models/Robot.js
@@ -1,36 +1,23 @@
 const { coordinateVectors, robotTurns } = require("../utils/index")
 
 const Robot = function (start, orientation) {
+    this.orientation = orientation
+    this.vector = coordinateVectors(orientation)
+    this.currentPosition = start
+}
 
-    var module = function () { }
+Robot.prototype.getCurrentStatus = function () {
+    return `${this.currentPosition.join(" ")} ${this.orientation}`
+}
 
-    module.orientation = orientation
-    module.vector = coordinateVectors(orientation)
-    module.currentPosition = start
-
-    module.getCurrentStatus = function () {
-        return `${module.currentPosition.join(" ")} ${module.orientation}`
+Robot.prototype.execInstruction = function (step) {
+    // console.log(module.vector)
+    if (step !== "F") {
+        this.orientation = robotTurns(this.orientation, step)
+        this.vector = coordinateVectors(this.orientation)
     }
-
-    module.execInstruction = function (step) {
-        // console.log(module.vector)
-        if (step !== "F") {
-            module.orientation = robotTurns(module.orientation, step)
-            module.vector = coordinateVectors(module.orientation)
-        }
-        else module.currentPosition = [module.currentPosition[0] + module.vector[0], module.currentPosition[1] + module.vector[1]]
-        // console.log(module.currentPosition)
-
-    }
-
-
-
-
-
-    return {
-        getCurrentStatus: module.getCurrentStatus,
-        execInstruction: module.execInstruction
-    };
+    else this.currentPosition = [this.currentPosition[0] + this.vector[0], this.currentPosition[1] + this.vector[1]]
+    // console.log(module.currentPosition)
 }
 
 module.exports = Robot
